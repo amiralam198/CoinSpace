5f7cb401d26c938b579748dc361a8dc3e52b6827
amiralam198 <157512945+amiralam198@users.noreply.github.com>
2025-09-06 12:46:46 +0530

    commit main app structure and update app.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+import './index.css'
+import CurrencyContext from './CurrencyContext.jsx'
+import 'react-alice-carousel/lib/alice-carousel.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <CurrencyContext>
+      <App />
+    </CurrencyContext>
   </StrictMode>,
 )
