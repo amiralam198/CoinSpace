d63ae06faba986a11450ef43986ac2193480773b
amiralam198 <157512945+amiralam198@users.noreply.github.com>
2025-09-06 20:54:13 +0530

    comment header

---
+++ src/components/Header.jsx
@@ -0,0 +1,56 @@
+import { AppBar, Container, createTheme, MenuItem, Select, ThemeProvider, Toolbar, Typography } from '@mui/material'
+import React from 'react'
+import {styled} from '@mui/system'
+import { CurrencyState } from '../CurrencyContext'
+import { useNavigate } from 'react-router-dom'
+
+const Header = () => {
+    const navigate = useNavigate();
+
+    const Title = styled(Typography)(({ theme }) => ({
+        flex: 1,
+        color: "gold",
+        fontFamily: "Montserrat",
+        fontWeight: "bold",
+        cursor: "pointer",
+    }))
+    
+    const darkTheme = createTheme({
+        palette: {
+            primary: {
+                main: '#fff'
+            },
+            mode: 'dark'
+        }
+    })
+
+    const {currency, setCurrency} = CurrencyState();
+
+    const handleClick = () => {
+        navigate('/');
+    }
+    
+  return (
+    <ThemeProvider theme={darkTheme}>
+    <AppBar color='transparent' position='static'>
+        <Container>
+            <Toolbar>
+                <Title onClick={handleClick}>
+                    Coin Space
+                </Title>
+                <Select variant='outlined'
+                        sx={{ width: 100, height: 40, marginLeft: 2 }}
+                        value={currency}
+                        onChange={(e) => setCurrency(e.target.value)}
+                >
+                    <MenuItem value={'USD'}>USD</MenuItem>
+                    <MenuItem value={'INR'}>INR</MenuItem>
+                </Select>
+            </Toolbar>
+        </Container>
+    </AppBar>
+    </ThemeProvider>
+  )
+}
+
+export default Header
